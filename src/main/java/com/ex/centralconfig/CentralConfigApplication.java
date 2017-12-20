a114b7456bf83ed48b33c0bb58706ebace7d3272
August Duet <augustduet@Augusts-MBP.home>
2017-12-20 16:23:26 -0500

    add the config server first commit, include manifest.yml for PCF

---
+++ src/main/java/com/ex/centralconfig/CentralConfigApplication.java
@@ -0,0 +1,14 @@
+package com.ex.centralconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CentralConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralConfigApplication.class, args);
+	}
+}
